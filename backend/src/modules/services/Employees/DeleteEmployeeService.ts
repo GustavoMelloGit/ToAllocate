40234f374b4575ef1b53e78ec8a5e9d8f5a225da
caio <caiofelicio17@gmail.com>
2021-12-18 23:36:36 -0300

    feat: adding verification of which projects are managed by the employee before deletion

--- backend/src/modules/services/Employees/DeleteEmployeeService.ts
+++ backend/src/modules/services/Employees/DeleteEmployeeService.ts
@@ -3,11 +3,28 @@ import { cursor } from "../../../utils/cursor";
 
 class DeleteEmployeeService {
   async execute(id: string) {
+    const employeeProjects = await cursor.query(`
+        SELECT project_name FROM project WHERE manager = '${id}'
+    `);
+
+    if (employeeProjects.rowCount > 0) {
+      let projectsNames: any = [];
+      employeeProjects.rows.forEach((row) => {
+        projectsNames.push(row.project_name);
+      });
+      throw new AppError(
+        `The employee is a manager in the following project(s): ${projectsNames.join(
+          ", "
+        )}. Please, update the project(s) manager before deleting the employee.`,
+        400
+      );
+    }
+
     const { rows } = await cursor.query(
-      `DELETE FROM employee WHERE id = '${id}' RETURNING Fname, Lname`
+      `SELECT FROM employee WHERE id = '${id}' RETURNING *`
     );
 
-    if (rows.length === 0) throw new AppError("Employee not found", 404);
+    if (rows.length === 0) throw new AppError("Employee not found");
 
     return rows[0];
   }
