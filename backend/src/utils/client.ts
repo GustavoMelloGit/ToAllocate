56eae7472728bb21f2e769ffbacbe7139763ecb5
caio <caiofelicio17@gmail.com>
2021-12-18 22:17:30 -0300

    refactor: editing the client.ts file to use environment variables

--- backend/src/utils/client.ts
+++ backend/src/utils/client.ts
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const client = {
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'password',
-  database: 'toallocate',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 };
 
 export { client };
