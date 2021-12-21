07cf90ad1b828ba20a66bad8297a21862a3106bb
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2021-12-20 21:44:27 -0300

    feat: logout added

--- frontend/src/components/layout/Header/index.tsx
+++ frontend/src/components/layout/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
 import { HeaderContainer } from './styles';
 import LogoutButtonComponent from './sub_components/LogoutButton';
 
@@ -7,10 +8,15 @@ interface IHeaderComponent {
 }
 
 export default function HeaderComponent(props: IHeaderComponent): JSX.Element {
+  const auth = useAuth();
+
+  const handleLogout = () => {
+    auth.logout();
+  };
   const { title } = props;
   return (
     <HeaderContainer>
-      <LogoutButtonComponent title='Logout' />
+      <LogoutButtonComponent title='Logout' onClick={handleLogout} />
       <h1>{title}</h1>
     </HeaderContainer>
   );
