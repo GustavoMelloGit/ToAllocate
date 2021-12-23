644b2b398fbe5f9efa1bfaac1f9fed905f821a25
caio <caiofelicio17@gmail.com>
2021-12-22 22:44:32 -0300

    refactor: format files

--- backend/src/utils/cursor.ts
+++ backend/src/utils/cursor.ts
@@ -1,5 +1,5 @@
-import { Pool } from 'pg';
-import { client } from './client';
+import { Pool } from "pg";
+import { client } from "./client";
 
 const cursor = new Pool(client);
 export { cursor };
