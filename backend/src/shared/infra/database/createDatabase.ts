2a003ca646e3acea008c63df8d6e847b2d11bcaa
caio <caiofelicio17@gmail.com>
2021-12-29 21:39:40 -0300

    feat: adding fake users when the project starts

--- backend/src/shared/infra/database/createDatabase.ts
+++ backend/src/shared/infra/database/createDatabase.ts
@@ -2,21 +2,22 @@ import { hashSync } from "bcryptjs";
 import { v4 } from "uuid";
 import { cursor } from "../../../utils/cursor";
 import AppError from "../../errors/AppError";
+import { users } from "../../../../fake_data";
 
 async function createDatabase() {
   try {
     await cursor.query(`
-  CREATE TABLE IF NOT EXISTS employee (
-      id uuid PRIMARY KEY,
-      Fname varchar(255) NOT NULL,
-      Lname varchar(255) NOT NULL,
-      cpf varchar(11) NOT NULL UNIQUE,
-      role varchar(20) NOT NULL,
-      email varchar(255) NOT NULL UNIQUE,
-      password varchar(64) NOT NULL, 
-      created_at TIMESTAMP DEFAULT NOW(),
-      updated_at TIMESTAMP DEFAULT NOW()
-  );
+      CREATE TABLE IF NOT EXISTS employee (
+          id uuid PRIMARY KEY,
+          Fname varchar(255) NOT NULL,
+          Lname varchar(255) NOT NULL,
+          cpf varchar(11) NOT NULL UNIQUE,
+          role varchar(20) NOT NULL,
+          email varchar(255) NOT NULL UNIQUE,
+          password varchar(64) NOT NULL, 
+          created_at TIMESTAMP DEFAULT NOW(),
+          updated_at TIMESTAMP DEFAULT NOW()
+      );
 `);
   } catch (error) {
     throw new AppError("Error during create table employee");
@@ -24,18 +25,18 @@ async function createDatabase() {
 
   try {
     await cursor.query(`
-        CREATE TABLE IF NOT EXISTS project (
-            project_id uuid PRIMARY KEY,
-            project_name varchar(50) NOT NULL UNIQUE,
-            start_date DATE NOT NULL,
-            end_date DATE NOT NULL,
-            cost FLOAT NOT NULL,
-            description varchar(500) NOT NULL,
-            manager varchar(11) NOT NULL,
-            images TEXT ARRAY[3],
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-        )
+      CREATE TABLE IF NOT EXISTS project (
+          project_id uuid PRIMARY KEY,
+          project_name varchar(50) NOT NULL UNIQUE,
+          start_date DATE NOT NULL,
+          end_date DATE NOT NULL,
+          cost FLOAT NOT NULL,
+          description varchar(500) NOT NULL,
+          manager varchar(11) NOT NULL,
+          images TEXT ARRAY[3],
+          created_at TIMESTAMP DEFAULT NOW(),
+          updated_at TIMESTAMP DEFAULT NOW()
+      )
     `);
   } catch (error) {
     throw new AppError("Error during create table project");
@@ -43,13 +44,13 @@ async function createDatabase() {
 
   try {
     await cursor.query(`
-        CREATE TABLE IF NOT EXISTS works_on (
-            employee_cpf varchar(11) NOT NULL,
-            project_id uuid NOT NULL,
-            ocuppation varchar(10) NOT NULL,
-            FOREIGN KEY (employee_cpf) REFERENCES employee(cpf) ON DELETE CASCADE,
-            FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE
-        )
+      CREATE TABLE IF NOT EXISTS works_on (
+          employee_cpf varchar(11) NOT NULL,
+          project_id uuid NOT NULL,
+          ocuppation varchar(10) NOT NULL,
+          FOREIGN KEY (employee_cpf) REFERENCES employee(cpf) ON DELETE CASCADE,
+          FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE
+      )
     `);
   } catch (error) {
     throw new AppError("Error during create table works_on");
@@ -57,11 +58,11 @@ async function createDatabase() {
 
   try {
     await cursor.query(`
-        CREATE TABLE IF NOT EXISTS token (
-            employee_id uuid NOT NULL,
-            token varchar(300),
-            FOREIGN KEY (employee_id) REFERENCES employee(id) ON DELETE CASCADE
-        )
+      CREATE TABLE IF NOT EXISTS token (
+          employee_id uuid NOT NULL,
+          token varchar(300),
+          FOREIGN KEY (employee_id) REFERENCES employee(id) ON DELETE CASCADE
+      )
   `);
   } catch (error) {
     throw new AppError("Error during create table token");
@@ -70,26 +71,52 @@ async function createDatabase() {
   try {
     // insere um usuario e admin e ignora se ja existe
     await cursor.query(`
-  INSERT INTO employee (
-      id,
-      Fname,
-      Lname,
-      cpf,
-      role,
-      email,
-      password        
-  ) VALUES (
-      '${v4()}',
-      'Admin',
-      'Admin',
-      '12345678912',
-      'admin',
-      'admin@admin.com',
-      '${hashSync("admin_password", 10)}'
-  ) ON CONFLICT DO NOTHING; 
-`);
+      INSERT INTO employee (
+          id,
+          Fname,
+          Lname,
+          cpf,
+          role,
+          email,
+          password        
+      ) VALUES (
+          '${v4()}',
+          'Admin',
+          'Admin',
+          '12345678912',
+          'admin',
+          'admin@admin.com',
+          '${hashSync("admin_password", 10)}'
+      ) ON CONFLICT DO NOTHING; 
+  `);
   } catch (error) {
     throw new AppError("Error during create admin user");
+  }
+
+  try {
+    users.forEach(async (user) => {
+      await cursor.query(`
+        INSERT INTO employee (
+            id,
+            Fname,
+            Lname,
+            cpf,
+            role,
+            email,
+            password        
+        ) VALUES (
+            '${v4()}',
+            '${user.Fname}',
+            '${user.Lname}',
+            '${user.cpf}',
+            '${user.role}',
+            '${user.email}',
+            '${hashSync(user.password, 10)}'
+        ) ON CONFLICT DO NOTHING; 
+    `);
+    });
+  } catch (error) {
+    throw new AppError("Error during create users");
   }
 }
 
